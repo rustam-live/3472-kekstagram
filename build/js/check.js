ce607fb1bdc57c9e8399233f3caffa300f509098
rustam-live <moltuganov@gmail.com>
2016-04-09 06:54:09 +0500

    Revert "Модуль 1 Задание 2"
    This reverts commit e117e70a9b6e10a34be674f1768601abf13444d3.

--- build/js/check.js
+++ build/js/check.js
@@ -2,24 +2,24 @@
 var getMessage = function(a, b) {
   if (typeof a === 'boolean') {
         if (a === true) {
-          return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-    }
+            return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+        }
         else {
-          return 'Переданное GIF-изображение не анимировано';
-    }
+            return 'Переданное GIF-изображение не анимировано';
+        }
     }
         if (typeof a === 'number') {
-          return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+            return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+    }
+        if (Array.isArray(a)) {
+          var sum = a.reduce(function(start, item) { return start + item; }, 0);
+          return 'Количество красных точек во всех строчках изображения: ' + sum;
     }
         if (Array.isArray(a) && Array.isArray(b)) {
           var square = 0;
           for (var i = 0; i < a.length && i < b.length; i++) {
           square = square + a[i] * b[i];
     }
-          return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
-    }
-        if (Array.isArray(a)) {
-          var sum = a.reduce(function(start, item) { return start + item; }, 0);
-          return 'Количество красных точек во всех строчках изображения: ' + sum;
-    }
+        return 'Общая площадь артефактов сжатия: ' + square + ' пикселей';
+  }
 }
